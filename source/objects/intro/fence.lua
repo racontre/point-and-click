4bb85d32cd90c515b86f6cc20055d5f1eef3cb2d
racontre <hurjaukko@gmail.com>
2020-05-03 13:47:35 +0300

    аааа

---
+++ source/objects/intro/fence.lua
@@ -0,0 +1,14 @@
+anim8 = require 'source.lib.anim8.anim8'
+
+	grid = anim8.newGrid(32, 32, 580, 121)
+fenceObject =		--proposing passing "objects" rather than a set of arguments? 
+{
+	name = 'wall',
+	colides = true,
+	destructible = false,
+	colImg = 'img/fenceCol.png',
+	image = 'img/keycol.png',
+	animation = anim8.newAnimation(grid('1-8',1), 0.1),
+	comment = 'We can\'t go any further. '
+}
+
